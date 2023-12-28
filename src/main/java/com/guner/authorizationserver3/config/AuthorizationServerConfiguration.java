5d592daf788028074b33a264c2ba33f08e7ac9a3
Ramazan Guner <ramazan.guner@haud.com>
2023-12-28 09:35:10 +0300

    removed comments

--- src/main/java/com/guner/authorizationserver3/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/guner/authorizationserver3/config/AuthorizationServerConfiguration.java
@@ -23,16 +23,6 @@ import java.util.UUID;
 @Configuration
 public class AuthorizationServerConfiguration {
 
-    /*
-     * auth code almak için
-     * http://localhost:8000/oauth2/authorize?response_type=code&client_id=client1&redirect_uri=http://127.0.0.1:8080/authorized&scope=openid%20read
-     *
-     * user/password girersen su adrese yönlendirir:
-     * http://127.0.0.1:8080/authorized?code=RVgy8JF8RbFdnHMmuazNxioVFvpiLShRfO7PUiJCBVHuo8jVf_-VpMlmy_dF9yOJL5LHOAqBlec1PilgEiuICwiU9WJ2FyX_9galbN58-41_tmACdkJAgxz_bhQVLbR8
-     *
-     * buradan gelen code değeri ile postman'de localhost:8000/oauth2/token isteğine code= olarak form parametresi iletilir.
-     */
-
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
         
